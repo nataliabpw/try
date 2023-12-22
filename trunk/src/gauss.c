32738f95d274852c112b5708731bc31fe9ffbc29
nomilknotever <01187598@pw.edu.pl>
2023-12-22 10:32:06 +0100

    wersja 2 funkcji w gauss.c

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -2,6 +2,28 @@
 #include "gauss.h"
 
 int diag(Matrix *mat, Matrix  *b, int c){
+	int i;
+	double tmp;
+	double max=mat->data[c][c];
+	int max_id=c;
+	for(i=c; i<mat->c; i++)
+		if (fabs(mat->data[i][c])>max){
+			max=fabs(mat->data[i][c]);
+			max_id=i;
+		}
+	for(i=c; i<mat->c; i++){
+		tmp=mat->data[c][i];
+		mat->data[c][i]=mat->data[max_id][i];
+		mat->data[max_id][i]=tmp;
+	}
+	for (i=0; i<b->c; i++){
+		tmp=b->data[c][i];
+		b->data[c][i]=b->data[max_id][i];
+		b->data[max_id][i]=tmp;
+	}
+	if (max==0.0)
+		return 1;
+	
 	return 0;
 }
 /**
@@ -9,25 +31,34 @@ int diag(Matrix *mat, Matrix  *b, int c){
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-	int c,r,w,ic,x;
-	double pom;
-	w=0;
-	for (c=0; c<mat->c; c++){
-		x=diag( mat, b, c);
-		if (x==1)
-			return 1;
-		for (r=w+1; r<mat->r; r++){
-			pom=mat->data[r][c];
-			for (ic=c; ic<mat->c; ic++){
-				mat->data[r][ic]-=pom*mat->data[c][ic]/mat->data[c][c];
-			}
-			for(ic=0; ic<b->c; ic++)
-				b->data[r][ic]=b->data[r][ic]-pom*b->data[w][ic]/mat->data[w][c];
-		}
-		w++;
-	}
-	
-	return 0;
+	int c, r, w, ic;
+    double pom;
+
+    w = 0;
+
+    for (c = 0; c < mat->c; c++) {
+        int x = diag(mat, b, c);
+
+        if (x == 1) {
+            return 1;
+        }
+
+        for (r = w + 1; r < mat->r; r++) {
+            pom = mat->data[r][c];
+
+            for (ic = c; ic < mat->c; ic++) {
+                mat->data[r][ic] -= pom * mat->data[c][ic] / mat->data[c][c];
+            }
+
+            for (ic = 0; ic < b->c; ic++) {
+                b->data[r][ic] -= pom * b->data[w][ic] / mat->data[w][c];
+            }
+        }
+
+        w++;
+    }
+
+    return 0;
 
 }
 
